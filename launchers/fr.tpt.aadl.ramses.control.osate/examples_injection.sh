898b53bf014cac7bddc1524c0b9b07df1636ac83
gardoll <sebastien.gardoll@telecom-paristech.fr>
2013-05-06 15:14:15 +0000

    * (examples_injection.sh):    - Copy the examples located in junit_tests into fr.tpt.aadl.ramses.control.osate so as to use RAMSES-OSATE example wizard.

---
+++ launchers/fr.tpt.aadl.ramses.control.osate/examples_injection.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# AADL-RAMSES
+ 
+# Copyright © 2013 TELECOM ParisTech and CNRS
+ 
+# TELECOM ParisTech/LTCI
+ 
+# Authors: see AUTHORS
+  
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the Eclipse Public License as published by Eclipse,
+# either version 1.0 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Eclipse Public License for more details.
+# You should have received a copy of the Eclipse Public License
+# along with this program.  If not, see 
+# http://www.eclipse.org/org/documents/epl-v10.php
+
+# Naming
+
+SCRIPT_DIR=`dirname $0`; cd ${SCRIPT_DIR}; SCRIPT_DIR=`pwd`
+TESTS_ROOT_DIRECTORY="${SCRIPT_DIR}/../../../../build_and_test/junit_tests/tests"
+EXAMPLE_DIRECTORY="${SCRIPT_DIR}/examples"
+OUTPUTREF_DIR_NAME="output_ref"
+CLEAN="FALSE"
+CP_OPTIONS="-rp"
+
+if [ "$1" == "--force" ]
+   then CLEAN="TRUE"
+   else CP_OPTIONS="${CP_OPTIONS}i"
+fi   
+
+if [ CLEAN == "TRUE" ] ; then
+  rm -fr ${EXAMPLE_DIRECTORY}
+fi
+
+mkdir -p ${EXAMPLE_DIRECTORY}
+cp ${CP_OPTIONS} ${TESTS_ROOT_DIRECTORY}/* ${EXAMPLE_DIRECTORY}
+
+find ${EXAMPLE_DIRECTORY} -type d -name "${OUTPUTREF_DIR_NAME}" -exec rm -fr {} \;
+
+echo "Examples for RAMSES-OSATE have been sucessfuly copied."
