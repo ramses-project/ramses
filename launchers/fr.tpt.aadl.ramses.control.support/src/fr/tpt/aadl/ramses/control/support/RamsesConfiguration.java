f5c5f2fb710f31964ad74ca499b4567a00e43cbf
borde <etienne.borde@telecom-paristech.fr>
2013-07-16 09:47:21 +0000

    add facility to retreive user code source folders

--- launchers/fr.tpt.aadl.ramses.control.support/src/fr/tpt/aadl/ramses/control/support/RamsesConfiguration.java
+++ launchers/fr.tpt.aadl.ramses.control.support/src/fr/tpt/aadl/ramses/control/support/RamsesConfiguration.java
@@ -1,13 +1,24 @@
 package fr.tpt.aadl.ramses.control.support;
 
 import java.io.File ;
+import java.io.IOException;
 import java.util.ArrayList ;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List ;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.osate.aadl2.NamedElement;
+import org.osate.aadl2.ProcessImplementation;
+import org.osate.aadl2.instance.InstanceObject;
 
 public class RamsesConfiguration
 {
   private static File OutputDirectory;
-  private static List<File> IncludeDirectories = new ArrayList<File>();
+  private static Map<Resource,Set<File>> IncludeDirectories = new LinkedHashMap<Resource,Set<File>>();
   private static File RamsesRessourcesDir;
   private static File InputDirectory;
   private static InstantiationManager _instantiationManager = new InstantiationManagerImpl();
@@ -28,9 +39,41 @@ public class RamsesConfiguration
     OutputDirectory = outputDir ;
   }
   
-  protected static void addIncludeDir(File includeDir)
+  public static void setIncludeDir(Resource r, Set<File> includeDirSet)
   {
-    IncludeDirectories.add(includeDir) ;
+	if(IncludeDirectories.containsKey(r))
+	  IncludeDirectories.get(r).addAll(includeDirSet) ;
+	else
+	{
+		includeDirSet.add(RamsesRessourcesDir);
+	  File periodicDelayedRuntimeDir;
+	  try {
+		
+		// Add dir in which the input resource was defined
+		URI uri = r.getURI();
+		String filePath = r.getURI().toFileString();
+		int lastIndex = filePath.indexOf(uri.lastSegment());
+		File inputResourceDir = new File(filePath.substring(0, lastIndex));
+		includeDirSet.add(inputResourceDir);
+		
+		// Add Dir of PeriodicDelayed runtime
+		periodicDelayedRuntimeDir = new File(
+				  RamsesRessourcesDir.getCanonicalPath()+"aadl_resources/C_runtime/PeriodicDelayed_rutime/");
+		includeDirSet.add(periodicDelayedRuntimeDir);
+		
+		// TODO: include other runtime directories here.
+		String pokPath = System.getenv("POK_PATH");
+		if(pokPath==null || pokPath=="")
+			pokPath = System.getProperty("POK_PATH");
+		File pokFile = new File(pokPath+"/libpok/include");
+		includeDirSet.add(pokFile);
+		
+	  } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	  }
+	  IncludeDirectories.put(r, includeDirSet);
+	}
   }
   
   protected static void setRamsesResourcesDir(File ramsesResourcesDir)
@@ -43,9 +86,14 @@ public class RamsesConfiguration
     return OutputDirectory;
   }
 
-  protected static List<File> getIncludeDir()
+  public static Set<File> getIncludeDir(Resource r)
   {
-    return IncludeDirectories;
+	if(IncludeDirectories.get(r)==null)
+	{
+	  Set<File> includeSet = new LinkedHashSet<File>();
+	  IncludeDirectories.put(r,includeSet);
+	}
+	return IncludeDirectories.get(r);
   }
   
   public static File getRamsesResourcesDir()
