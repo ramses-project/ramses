0cd178815c837be15d7d3f4ec97dc97366f0ee0a
gardoll <sebastien.gardoll@telecom-paristech.fr>
2014-03-10 20:45:42 +0000

    * (EnvUtils.java):   - remove log4j dependency for JUnit tests.

--- launchers/fr.tpt.aadl.ramses.control.support/src/fr/tpt/aadl/ramses/control/support/EnvUtils.java
+++ launchers/fr.tpt.aadl.ramses.control.support/src/fr/tpt/aadl/ramses/control/support/EnvUtils.java
@@ -1,25 +1,14 @@
 package fr.tpt.aadl.ramses.control.support;
 
-import org.apache.log4j.Logger ;
-
 public class EnvUtils
 {
-  private static Logger _LOGGER = Logger.getLogger(EnvUtils.class) ; 
-  
   public static String getEnvVariable(String envVarName)
   {
     String result = null ;
     
     if(envVarName != null)
     {
-      result=System.getenv(envVarName);
-      
-      if(result==null || result=="")
-      { 
-        result = System.getProperty(envVarName);
-        _LOGGER.trace("\'$" + envVarName + "\' environment variable returns: \'"+
-                      result + '\'');
-      }
+      result=System.getenv(envVarName);      
     }
 
     return result ;
