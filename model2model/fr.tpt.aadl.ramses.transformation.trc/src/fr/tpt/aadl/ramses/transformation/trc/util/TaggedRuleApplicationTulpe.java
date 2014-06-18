611c2bafd08a17cf1b821660e1c5ab1be50f89ef
smail <smail@sma.(none)>
2014-06-18 15:54:16 +0200

    cleaning the project atl to trc handler

---
+++ model2model/fr.tpt.aadl.ramses.transformation.trc/src/fr/tpt/aadl/ramses/transformation/trc/util/TaggedRuleApplicationTulpe.java
@@ -0,0 +1,28 @@
+package fr.tpt.aadl.ramses.transformation.trc.util;
+
+
+public class TaggedRuleApplicationTulpe extends RuleApplicationTulpe {
+
+
+boolean _exclusion=false;
+
+
+public boolean isExclusion()
+
+{
+
+return _exclusion ;
+
+}
+
+
+public void setExclusion(boolean _exclusion)
+
+{
+
+this._exclusion = _exclusion ;
+
+}
+
+
+}
