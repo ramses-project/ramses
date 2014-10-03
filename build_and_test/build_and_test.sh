bf01adee1b968ebf0d8e23476e99aadb672e3b79
Sebastien Gardoll <sebastien.gardoll@telecom-paristech.fr>
2014-10-03 18:35:41 +0200

    * (build_and_test.sh):    - Add usage.

--- build_and_test/build_and_test.sh
+++ build_and_test/build_and_test.sh
@@ -27,6 +27,12 @@ LOG_FILE='test.log'
 
 ################################## MAIN ########################################
 
+if [ $1 -z ]
+  then
+    echo -e 'linux or mac ?'
+    exit -1
+fi
+
 case $1 in
   'linux') ZIP_FILE=${ZIP_FILE_PREFIX}${LINUX_SUFFIX} ;;
   'mac') ZIP_FILE=${ZIP_FILE_PREFIX}${MAC_SUFFIX} ;;
