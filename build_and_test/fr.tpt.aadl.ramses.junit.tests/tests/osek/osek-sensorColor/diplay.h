b502ff3e3e4e9820faa08805b3a575aee2393ad6
Cuauhtemoc Castellanos <cuauhtemoc.castellanos@telecom-paristech.fr>
2015-01-26 16:30:18 +0100

    Aadl model of color sensor test

---
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-sensorColor/diplay.h
@@ -0,0 +1,20 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+
+
+/*********************/
+/* Ungenerated datas */
+/*********************/
+
+#include "ecrobot_interface.h"
+
+void myDisplay(U16 currentColor);
+void plop();
+
+void ecrobot_device_initialize(void);
+void ecrobot_device_terminate(void);
+
+#endif
