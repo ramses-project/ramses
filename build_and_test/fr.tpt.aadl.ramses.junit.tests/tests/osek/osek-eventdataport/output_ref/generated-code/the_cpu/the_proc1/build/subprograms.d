d1d94bf60b54abe9aa4301ac7efa0f57cc81279a
Cuauhtemoc Castellanos <cuauhtemoc.castellanos@telecom-paristech.fr>
2015-02-03 13:48:53 +0100

    evnet dataport example output reference

---
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-eventdataport/output_ref/generated-code/the_cpu/the_proc1/build/subprograms.d
@@ -0,0 +1,14 @@
+build/subprograms.o: subprograms.c \
+ /home/castella/tools/ramses-install-test/../../../../opt/repositories/ramses-develop/model2model/fr.tpt.aadl.ramses.transformation.atl/aadl_resources/C_runtime/OSEK/osek_runtime.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/include/kernel.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/config/at91sam7s-gnu/lego_nxt/sys_defs.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/config/at91sam7s-gnu/cpu_defs.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/include/osek.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/include/t_stddef.h \
+ /home/castella/sandbox/nxtOSEK-ramses/ecrobot/../toppers_osek/config/at91sam7s-gnu/tool_defs.h \
+ subprograms.h \
+ /home/castella/workspaces/runtime-New_configuration2/eventdataport/input/user_receive.h \
+ /home/castella/workspaces/runtime-New_configuration2/eventdataport/input/user_send.h \
+ main.h kernel_id.h gtypes.h \
+ /home/castella/sandbox/nxtOSEK-ramses/toppers_osek/include/kernel.h \
+ gtypes.h main.h
