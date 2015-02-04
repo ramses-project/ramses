9165ee2fc97bacb828c62c3658e507740fb42077
Cuauhtemoc Castellanos <cuauhtemoc.castellanos@telecom-paristech.fr>
2015-02-04 11:27:37 +0100

    modified to inherit from OSGIScenario

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/src/test/java/ramses/test/cli/osek/Test_JUnit_dataport.java
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/src/test/java/ramses/test/cli/osek/Test_JUnit_dataport.java
@@ -6,8 +6,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ramses.test.util.OSEKScenario;
+import ramses.test.util.OSGIOSEKScenario;
 
-public class Test_JUnit_dataport extends OSEKScenario{
+public class Test_JUnit_dataport extends OSGIOSEKScenario{
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -17,30 +18,13 @@ public class Test_JUnit_dataport extends OSEKScenario{
 
 	@Test
 	public void Test_JUnit_dataport_exec() {
-		
-		this.exec();
-//		fail("Not yet implemented");
-	}
-
-	@Override
-	protected Process executeGeneratedCode() throws Exception {	
-		Runtime runtime = Runtime.getRuntime();
-		String[] testCommandArray = {"sleep", "3"};
-		Process generatedCodeExecProcess = runtime.exec(testCommandArray);
-		Thread.sleep(4000);
-		return generatedCodeExecProcess;
-	}
-
-	@Override
-	protected void init() {
 		input = "osek-dataport/input/test_osek_dataports.aadl2,";
 		includeList = "tests/osek/";
 		ramses_dir = System.getProperty("RAMSES_DIR");
 		output = "tests/osek/osek-dataport/output";
 		output_ref = "tests/osek/osek-dataport/output_ref";
-		target= "osek";
 		system_impl = "root.impl";
-		
+		this.exec();
 	}
 
 	@Override
