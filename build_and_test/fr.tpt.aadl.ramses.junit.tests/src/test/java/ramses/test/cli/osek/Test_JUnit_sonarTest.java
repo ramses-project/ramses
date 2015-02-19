24797bec9306a576b1b6f46b165a9ba864cc03fc
Cuauhtemoc Castellanos <cuauhtemoc.castellanos@telecom-paristech.fr>
2015-02-19 10:22:57 +0100

    new test, osek sonar test case

---
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/src/test/java/ramses/test/cli/osek/Test_JUnit_sonarTest.java
@@ -0,0 +1,50 @@
+package ramses.test.cli.osek;
+
+import java.io.File;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ramses.test.util.OSGIOSEKScenario;
+
+public class Test_JUnit_sonarTest extends OSGIOSEKScenario{
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		
+	}
+
+	@Test
+	public void Test_JUnit_modes_exec() {
+		String root_path = "tests/osek/osek-sonar-test";
+		input = "osek-modes/input/model.aadl,";
+		includeList = "tests/osek/";
+		ramses_dir = System.getProperty("RAMSES_DIR");
+		output = root_path+"/output";
+		output_ref = root_path+"/output_ref";
+		system_impl = "root.impl";
+//		workflowPath = "osek-modes/input/modes.workflow";
+		
+	    File dir = new File(output);
+	    if (!dir.exists()) {
+	        System.out.println("creating directory: " + output);
+	        boolean result = false;
+	        try{
+	            dir.mkdir();
+	            result = true;
+	         } catch(SecurityException se){
+	        	 se.printStackTrace();
+	         }        
+	         if(result) {    
+	           System.out.println(output+" created");  
+	         }
+	      }
+		this.exec();
+	}
+
+	@Override
+	protected void initAdditionalParameters() {
+		// TODO Auto-generated method stub
+		
+	}
+}
