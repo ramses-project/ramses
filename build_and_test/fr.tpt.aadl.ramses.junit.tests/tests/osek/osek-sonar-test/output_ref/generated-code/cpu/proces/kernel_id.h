5d812972a6adb9f7e47366ab0cf4e5db3b7c5aca
root <root@lame2.enst.fr>
2015-04-28 13:49:51 +0200

    fix of false negative test result

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-sonar-test/output_ref/generated-code/cpu/proces/kernel_id.h
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-sonar-test/output_ref/generated-code/cpu/proces/kernel_id.h
@@ -1,6 +1,6 @@
 /*
  *  kernel_id.h
- *  Wed Mar 18 17:21:25 2015
+ *  Tue Apr 28 01:30:24 2015
  *  SG Version 2.00
  *  sg.exe ./proces.oil -os=ECC2 -I/data/packages/nxtOSEK/ecrobot/../toppers_osek/sg/impl_oil -template=/data/packages/nxtOSEK/ecrobot/../toppers_osek/sg/lego_nxt.sgt
  */
