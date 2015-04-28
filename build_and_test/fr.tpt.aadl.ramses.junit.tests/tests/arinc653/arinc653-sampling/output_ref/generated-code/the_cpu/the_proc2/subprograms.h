3cb4b079eecedcdf3de424fb545d8323ebd2bfff
root <root@lame2.enst.fr>
2015-04-28 14:59:16 +0200

    fix of false negative test result

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-sampling/output_ref/generated-code/the_cpu/the_proc2/subprograms.h
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-sampling/output_ref/generated-code/the_cpu/the_proc2/subprograms.h
@@ -6,7 +6,8 @@
 #include "user_code.h"
 #include "gtypes.h"
 #include "main.h"
-void test_sampling_refined_model__the_proc2_the_receiver_entrypoint_impl(SAMPLING_PORT_ID_TYPE *  p_in);
+void test_sampling_refined_model__the_proc2_the_receiver_entrypoint_impl(
+	SAMPLING_PORT_ID_TYPE *  p_in);
 
 typedef enum {
   test_sampling_refined_model__the_proc2_the_receiver_entrypoint_impl_BA_entrypoint_init_state,
