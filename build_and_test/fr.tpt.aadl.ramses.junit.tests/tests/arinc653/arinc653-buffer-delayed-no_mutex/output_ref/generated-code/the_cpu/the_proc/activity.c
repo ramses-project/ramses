828d2e9bd69fccb583e9290c9f178098ce019fec
root <root@lame2.enst.fr>
2015-08-31 18:25:48 +0200

    fix of false negative test result

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-buffer-delayed-no_mutex/output_ref/generated-code/the_cpu/the_proc/activity.c
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-buffer-delayed-no_mutex/output_ref/generated-code/the_cpu/the_proc/activity.c
@@ -2,18 +2,46 @@
 #include "main.h"
 
 extern test_buffer_delayed_refined_model__bufferArrayImpl_the_receiver_p_in_impl the_receiver_p_in_Buffer;
+test_buffer_delayed_refined_model__the_proc_the_sender_impl_BA_State_t the_proc_the_sender_impl_current_state = test_buffer_delayed_refined_model__the_proc_the_sender_impl_main_state;
 void* test_buffer_delayed_refined_model__the_proc_the_sender_impl_Job()
 {
-  while (1) {
-    test_buffer_delayed_refined_model__the_proc_the_sender_entrypoint_impl (&the_receiver_p_in_Buffer);
+  while(1)
+{
+  switch(the_proc_the_sender_impl_current_state)
+  {
+    case test_buffer_delayed_refined_model__the_proc_the_sender_impl_main_state:
+    // Transition id: dispatch_transition
+    if(1)
+    {
+      the_proc_the_sender_impl_current_state = test_buffer_delayed_refined_model__the_proc_the_sender_impl_main_state;
+      if (1) {
+      test_buffer_delayed_refined_model__the_proc_the_sender_entrypoint_impl (&the_receiver_p_in_Buffer);
+      }
+      return;
+    }
   }
-  return 0;
+}
+return 0;
 }
 extern test_buffer_delayed_refined_model__bufferArrayImpl_the_receiver_p_in_impl the_receiver_p_in_Buffer;
+test_buffer_delayed_refined_model__the_proc_the_receiver_impl_BA_State_t the_proc_the_receiver_impl_current_state = test_buffer_delayed_refined_model__the_proc_the_receiver_impl_main_state;
 void* test_buffer_delayed_refined_model__the_proc_the_receiver_impl_Job()
 {
-  while (1) {
-    test_buffer_delayed_refined_model__the_proc_the_receiver_entrypoint_impl (&the_receiver_p_in_Buffer);
+  while(1)
+{
+  switch(the_proc_the_receiver_impl_current_state)
+  {
+    case test_buffer_delayed_refined_model__the_proc_the_receiver_impl_main_state:
+    // Transition id: dispatch_transition
+    if(1)
+    {
+      the_proc_the_receiver_impl_current_state = test_buffer_delayed_refined_model__the_proc_the_receiver_impl_main_state;
+      if (1) {
+      test_buffer_delayed_refined_model__the_proc_the_receiver_entrypoint_impl (&the_receiver_p_in_Buffer);
+      }
+      return;
+    }
   }
-  return 0;
+}
+return 0;
 }
