314e8ed1e525bbb606822d8d434b577c960ec962
root <root@lame2.enst.fr>
2015-08-31 18:23:00 +0200

    fix of false negative test result

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-queuing/output_ref/generated-code/the_cpu/the_proc1/activity.c
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/arinc653/arinc653-queuing/output_ref/generated-code/the_cpu/the_proc1/activity.c
@@ -2,10 +2,24 @@
 #include "main.h"
 
 extern APEX_INTEGER the_proc1_p_out_globalVariable;
+test_queuing_refined_model__the_proc1_the_sender_impl_BA_State_t the_proc1_the_sender_impl_current_state = test_queuing_refined_model__the_proc1_the_sender_impl_main_state;
 void* test_queuing_refined_model__the_proc1_the_sender_impl_Job()
 {
-  while (1) {
-    test_queuing_refined_model__the_proc1_the_sender_entrypoint_impl (&the_proc1_p_out_globalVariable);
+  while(1)
+{
+  switch(the_proc1_the_sender_impl_current_state)
+  {
+    case test_queuing_refined_model__the_proc1_the_sender_impl_main_state:
+    // Transition id: dispatch_transition
+    if(1)
+    {
+      the_proc1_the_sender_impl_current_state = test_queuing_refined_model__the_proc1_the_sender_impl_main_state;
+      if (1) {
+      test_queuing_refined_model__the_proc1_the_sender_entrypoint_impl (&the_proc1_p_out_globalVariable);
+      }
+      return;
+    }
   }
-  return 0;
+}
+return 0;
 }
