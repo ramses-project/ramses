f844bbf4b2e3a42f834c25d364b0069403facd37
root <root@lame2.enst.fr>
2015-09-21 23:04:13 +0200

    fix of false negative test result

--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-eventport/output_ref/generated-code/the_cpu/the_proc1/build/deployment.d
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-eventport/output_ref/generated-code/the_cpu/the_proc1/build/deployment.d
@@ -6,5 +6,5 @@ build/deployment.o: deployment.c deployment.h main.h \
  /data/packages/nxtOSEK/ecrobot/../toppers_osek/include/t_stddef.h \
  /data/packages/nxtOSEK/ecrobot/../toppers_osek/config/at91sam7s-gnu/tool_defs.h \
  kernel_id.h gtypes.h \
- /data/packages/nxtOSEK/toppers_osek/include/kernel.h \
- /data/hudson/jobs/RAMSES_TEST/ramses-SNAPSHOT-bin/configuration/org.eclipse.osgi/31/0/.cp/aadl_resources/C_runtime/OSEK/osek_runtime.h
+ /data/hudson/jobs/RAMSES_TEST/ramses-SNAPSHOT-bin/configuration/org.eclipse.osgi/31/0/.cp/aadl_resources/C_runtime/OSEK/osek_runtime.h \
+ /data/packages/nxtOSEK/toppers_osek/include/kernel.h
--- build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-eventport/output_ref/generated-code/the_cpu/the_proc1/kernel_cfg.c
+++ build_and_test/fr.tpt.aadl.ramses.junit.tests/tests/osek/osek-eventport/output_ref/generated-code/the_cpu/the_proc1/kernel_cfg.c
@@ -1,6 +1,6 @@
 /*
  *  kernel_cfg.c
- *  Tue Apr 28 01:14:10 2015
+ *  Mon Sep 21 01:03:47 2015
  *  SG Version 2.00
  *  sg.exe ./the_proc1.oil -os=ECC2 -I/data/packages/nxtOSEK/ecrobot/../toppers_osek/sg/impl_oil -template=/data/packages/nxtOSEK/ecrobot/../toppers_osek/sg/lego_nxt.sgt
  */
